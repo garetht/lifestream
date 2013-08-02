47d06093f3f3141303402eadf8939f7adf799e71
Gareth Tan <garethwtan@gmail.com>
2013-08-02 10:13:22 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifestream::Application.config.secret_token = 'c93c6e7bc960991574b634b001fedba0c7fe25b48a113206f34d2a0c4e79740416a0a4c0d23fa923b9cd2af8911a0b26c7f6bb0d91890fb285ce1ae5a7384478'
