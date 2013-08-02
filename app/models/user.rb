a57496ca68f9e296535b2877bf39f9bbd88234cb
Gareth Tan <garethwtan@gmail.com>
2013-08-02 17:21:23 -0400

    Fix li link clicking

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  # attr_accessible :title, :body
+  attr_accessible :email, :password, :remember_me
 end
