048e585c365f6297f26f5d63e297481daac63f6f
Gareth Tan <garethwtan@gmail.com>
2013-08-08 21:54:49 -0400

    Add sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Lifestream::Application.initialize!
+
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+    ActionMailer::Base.delivery_method ||= :smtp
+elsif Rails.env.development?
+  # hey, did you hear about letter opener? Install it in your gemfile.
+  ActionMailer::Base.delivery_method = :letter_opener
+end
