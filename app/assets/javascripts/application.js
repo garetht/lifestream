877b3bbcbba5302729cdb3442eaeb1e90d6cf802
Gareth Tan <garethwtan@gmail.com>
2013-08-10 19:47:47 -0400

    Fix bug with magical replacements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,59 +16,6 @@
 //= require jquery_ujs
 
 /*
-  * LSParser-JS ~ 
-  * :: A simple parser that makes API calls to replace text surrounded
-  * :: in @  @. The tag, recall, is at-space!
-*/
-
-var LSParser = function(){
-  var parse = function(string){
-    console.log(string)
-    var lsStrings = string.match(/@ (.+) @/g);
-    var results = _.each(lsStrings, function(lsString){
-      parseSequence(lsString);
-    });
-  };
-
-  var parseSequence = function(lsString){
-    var split = lsString.match(/@ ([A-Z]+)(.*) @/);
-    var command = split[1]
-    var arguments = split[2].replace(/^\s+|\s+$/, '').split(" ");
-
-    if(command == "STOCK"){
-      parseStock(arguments);
-    }
-    else if(command == "WEATHER"){
-      parseWeather(arguments);
-    }
-    else{
-      parseIdentity(command, arguments);
-    }
-  };
-
-  var parseStock = function(arguments){
-    var ticker = arguments[0];
-    stockApiCall(ticker);
-  }
-
-  var stockApiCall = function(ticker){
-    var apiurl = "http://dev.markitondemand.com/Api/Quote";
-    $.get("http://localhost:3000/passthrough/get", 
-          {apiurl: apiurl, type: "xml", params: {symbol: ticker}})
-    .done(function(data){
-      console.log(data)
-      var price = data["QuoteApiModel"]["Data"]["LastPrice"];
-      if (price){
-        $("#markdown-edit").val($("#markdown-edit").val().replace(/@ STOCK.* @/, price));
-    }
-    });
-  };
-
-  return {parse: parse};
-}();
-
-
-/*
   * Konami-JS ~ 
   * :: Now with support for touch events and multiple instances for 
   * :: those situations that call for multiple easter eggs!
